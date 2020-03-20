4c231c19ffb6a1493be97600ebb1a67dc1f31711
hemina <mina.he@telecom-bretagne.eu>
2020-03-20 23:57:43 +0100

    day8

---
+++ problem_404.py
@@ -0,0 +1,17 @@
+"""
+Find the sum of all left leaves in a given binary tree.
+
+Example:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+
+https://leetcode.com/problems/sum-of-left-leaves/
+
+@author Mina HE
+"""
