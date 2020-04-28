a55b67d486c2414e72e35147fc96c4ca657d8980
hemina <mina.he@telecom-bretagne.eu>
2020-04-28 23:32:43 +0200

    day47

---
+++ problem_000.py
@@ -0,0 +1,107 @@
+"""
+You have a queue of integers, you need to retrieve the first unique integer in the queue.
+
+Implement the FirstUnique class:
+
+FirstUnique(int[] nums) Initializes the object with the numbers in the queue.
+int showFirstUnique() returns the value of the first unique integer of the queue,
+and returns -1 if there is no such integer.
+void add(int value) insert value to the queue.
+
+
+Example 1:
+
+Input:
+["FirstUnique","showFirstUnique","add","showFirstUnique","add","showFirstUnique","add","showFirstUnique"]
+[[[2,3,5]],[],[5],[],[2],[],[3],[]]
+Output:
+[null,2,null,2,null,3,null,-1]
+
+Explanation:
+FirstUnique firstUnique = new FirstUnique([2,3,5]);
+firstUnique.showFirstUnique(); // return 2
+firstUnique.add(5);            // the queue is now [2,3,5,5]
+firstUnique.showFirstUnique(); // return 2
+firstUnique.add(2);            // the queue is now [2,3,5,5,2]
+firstUnique.showFirstUnique(); // return 3
+firstUnique.add(3);            // the queue is now [2,3,5,5,2,3]
+firstUnique.showFirstUnique(); // return -1
+
+Example 2:
+
+Input:
+["FirstUnique","showFirstUnique","add","add","add","add","add","showFirstUnique"]
+[[[7,7,7,7,7,7]],[],[7],[3],[3],[7],[17],[]]
+Output:
+[null,-1,null,null,null,null,null,17]
+
+Explanation:
+FirstUnique firstUnique = new FirstUnique([7,7,7,7,7,7]);
+firstUnique.showFirstUnique(); // return -1
+firstUnique.add(7);            // the queue is now [7,7,7,7,7,7,7]
+firstUnique.add(3);            // the queue is now [7,7,7,7,7,7,7,3]
+firstUnique.add(3);            // the queue is now [7,7,7,7,7,7,7,3,3]
+firstUnique.add(7);            // the queue is now [7,7,7,7,7,7,7,3,3,7]
+firstUnique.add(17);           // the queue is now [7,7,7,7,7,7,7,3,3,7,17]
+firstUnique.showFirstUnique(); // return 17
+
+Example 3:
+
+Input:
+["FirstUnique","showFirstUnique","add","showFirstUnique"]
+[[[809]],[],[809],[]]
+Output:
+[null,809,null,-1]
+
+Explanation:
+FirstUnique firstUnique = new FirstUnique([809]);
+firstUnique.showFirstUnique(); // return 809
+firstUnique.add(809);          // the queue is now [809,809]
+firstUnique.showFirstUnique(); // return -1
+
+
+
+Constraints:
+
+1 <= nums.length <= 10^5
+1 <= nums[i] <= 10^8
+1 <= value <= 10^8
+At most 50000 calls will be made to showFirstUnique and add.
+
+https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/531/week-4/3313/
+
+@author Mina HE
+"""
+
+from collections import OrderedDict
+
+
+class FirstUnique:
+
+    def __init__(self, nums: List[int]):
+        self.d = OrderedDict()
+        for num in nums:
+            self.d[num] = self.d.get(num, 0) + 1
+        self.unique = [k for k, v in self.d.items() if v == 1]
+
+    def showFirstUnique(self) -> int:
+        return self.unique[0] if self.unique else -1
+
+    def add(self, value: int) -> None:
+        self.d[value] = self.d.get(value, 0) + 1
+        if self.d[value] == 1:
+            self.unique.append(value)
+        elif self.d[value] == 2:
+            self.unique.remove(value)
+
+# Your FirstUnique object will be instantiated and called as such:
+# obj = FirstUnique(nums)
+# param_1 = obj.showFirstUnique()
+# obj.add(value)
+
+
+"""
+Runtime: 2720 ms
+Memory Usage: 61.9 MB
+Complexity: O(n)
+"""
