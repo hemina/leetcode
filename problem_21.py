88c6cfbe6c6f597de0765130ad73e895a6419cc8
hemina <mina.he@telecom-bretagne.eu>
2020-10-16 22:07:15 +0200

    new start day 3

---
+++ problem_21.py
@@ -0,0 +1,57 @@
+"""
+Merge two sorted linked lists and return it as a new sorted list.
+The new list should be made by splicing together the nodes of the first two lists.
+
+
+
+Example 1:
+
+
+Input: l1 = [1,2,4], l2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: l1 = [], l2 = []
+Output: []
+Example 3:
+
+Input: l1 = [], l2 = [0]
+Output: [0]
+
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both l1 and l2 are sorted in non-decreasing order.
+
+https://leetcode.com/problems/merge-two-sorted-lists/
+
+@author Mina HE
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+
+"""
+Runtime: 40 ms, faster than 52.34% of Python online submissions.
+Memory Usage: 14.2 MB, less than 99.99% of Python online submissions.
+Complexity: O(n)
+"""
