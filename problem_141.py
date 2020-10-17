91c3fad0972ff5e97b0e9658a2cb8448aa338bb1
hemina <mina.he@telecom-bretagne.eu>
2020-10-17 21:04:23 +0200

    new start day 5

---
+++ problem_141.py
@@ -0,0 +1,72 @@
+"""
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again
+by continuously following the next pointer.
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to.
+Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+Follow up:
+
+Can you solve it using O(1) (i.e. constant) memory?
+
+
+
+Example 1:
+
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+Example 2:
+
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+Example 3:
+
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+
+
+Constraints:
+
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.
+
+https://leetcode.com/problems/linked-list-cycle/
+
+@author Mina HE
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        try:
+            slow = head
+            fast = head.next
+            while slow is not fast:
+                slow = slow.next
+                fast = fast.next.next
+            return True
+        except:
+            return False
+
+
+"""
+Runtime: 44 ms, faster than 91.29% of Python online submissions.
+Memory Usage: 17.1 MB, less than 99.00% of Python online submissions.
+Complexity: O(n)
+"""
