2d79d46e12a2d38bc6285f30f44f6ea3b4d048c4
hemina <mina.he@telecom-bretagne.eu>
2020-10-19 22:17:07 +0200

    new start day 7

---
+++ problem_112.py
@@ -0,0 +1,44 @@
+"""
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path such
+that adding up all the values along the path equals the given sum.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given the below binary tree and sum = 22,
+
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \      \
+7    2      1
+return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+https://leetcode.com/problems/path-sum/
+
+@author Mina HE
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        if not root.left and not root.right and sum == root.val:
+            return True
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
+
+
+"""
+Runtime: 44 ms, faster than 67.30% of Python online submissions.
+Memory Usage: 15.9 MB, less than 90.69% of Python online submissions.
+"""
